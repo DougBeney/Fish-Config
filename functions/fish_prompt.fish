1c3435e96b634516e7944fbab629ab6960f7eed6
Doug Beney <contact@dougie.io>
2019-08-18 19:27:46 -0400

    prompt tweaks

--- functions/fish_prompt.fish
+++ functions/fish_prompt.fish
@@ -1,5 +1,9 @@
+# Fish git prompt
+set __fish_git_prompt_showdirtystate 'yes'C
+set __fish_git_prompt_color_upstream_behind red
+
 function fish_prompt
     set_color green
-    printf "• "
+    printf "• %s/ " (basename (pwd))
     set_color normal
 end
