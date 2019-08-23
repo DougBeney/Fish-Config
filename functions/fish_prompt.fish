42b5874de32b0c4ce51d2f6417058d25b5f47771
Doug Beney <work@dougie.io>
2019-08-22 22:43:24 -0400

    fish git prompt

--- functions/fish_prompt.fish
+++ functions/fish_prompt.fish
@@ -1,9 +1,29 @@
 # Fish git prompt
-set __fish_git_prompt_showdirtystate 'yes'C
+set __fish_git_prompt_show_informative_status
+set __fish_git_prompt_showcolorhints
+set __fish_git_prompt_showupstream "informative"
+
+# Colors
+set green (set_color green)
+set magenta (set_color magenta)
+set normal (set_color normal)
+set red (set_color red)
+set yellow (set_color yellow)
+
+set __fish_git_prompt_color_branch magenta --bold
+set __fish_git_prompt_color_dirtystate white
+set __fish_git_prompt_color_invalidstate red
+set __fish_git_prompt_color_merging yellow
+set __fish_git_prompt_color_stagedstate yellow
+set __fish_git_prompt_color_upstream_ahead green
 set __fish_git_prompt_color_upstream_behind red
 
 function fish_prompt
     set_color green
-    printf "• %s/ " (basename (pwd))
+    if __fish_git_prompt > /dev/null
+        printf "• %s/ %s " (basename (pwd)) (__fish_git_prompt)
+    else
+        printf "• %s/ " (basename (pwd))
+    end
     set_color normal
 end
